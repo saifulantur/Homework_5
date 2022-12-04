40bda9c97a6e73f1686bf48ba031ec947025301d
Saiful Antur <saifulantur@gmail.com>
2022-12-04 19:02:03 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='snowflake',
     version='0.0.1',
-    description='My public snowflake package from private github repo',
+    description='My snowflake package from my github repository',
     author='Md Shiful Islam',
     author_email='saifulantur@gmail.com',
     packages=find_packages(),
