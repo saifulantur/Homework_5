12184cf070718fda1a5d3c8dec844ebb96b00092
Saiful Antur <saifulantur@gmail.com>
2022-12-04 18:22:29 +0600

    updated stepu.py and init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='snowflake',
+    version='0.0.1',
+    description='My public snowflake package from private github repo',
+    author='Md Shiful Islam',
+    author_email='saifulantur@gmail.com',
+    packages=['snowflake'],
+    zip_safe=False
+)
